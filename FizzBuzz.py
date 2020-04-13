5b3a7df4e625831434930c58097e0cbfc02342d6
Mikhail_Davydov <mdavydov83@gmail.com>
2020-04-13 23:54:12 +0300

    Upraznenie FizzBuzz gotovo (ver 0.1)

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,17 @@
 def fizz_buzz(first_num, last_num):
-    for i in range(first_num, last_num):
+    if first_num > last_num:
+        return ''
+    list1 = range(first_num, last_num + 1)
+    list = []
+    for i in list1:
         if i % 3 == 0 and i % 5 == 0:
             list.append('FizzBuzz')
         elif i % 3 == 0:
             list.append('Fizz')
         elif i % 5 == 0:
             list.append('Buzz')
-            list.append('i')
-        return list
+        else:
+            list.append(i)
+    return list
 
-fizz_buzz(3, 12)
+print(fizz_buzz(15, 18))
